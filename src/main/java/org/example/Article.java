3a53a1b5a1c7f571636d94112cab4c3233995997
insung5189 <insung5189@gmail.com>
2023-04-18 15:38:50 +0900

    수정된 JDBC 코드

--- src/main/java/org/example/Article.java
+++ src/main/java/org/example/Article.java
@@ -4,11 +4,15 @@ public class Article {
     public int id;
     public String title;
     public String body;
+    public String updateDate;
+    public String regDate;
 
-    public Article(int id, String title, String body) {
+    public Article(int id, String title, String body, String regDate, String updateDate) {
         this.id = id;
         this.title = title;
         this.body = body;
+        this.regDate = regDate;
+        this.updateDate = updateDate;
     }
     //@Override
     public String toString () {
